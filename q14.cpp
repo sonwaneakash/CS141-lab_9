8a299c39ccf7328548814a4f52c38a369354c3b0
sonwaneakash <42178604+sonwaneakash@users.noreply.github.com>
2018-11-10 00:02:30 +0530

    Create q14.cpp

---
+++ q14.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+#include<cstring>
+
+//main fn
+int main(){
+        string myname="Akash Sonwane";
+        cout<<"Printing string myname using indices method: ";
+        for(int i=0;myname[i]!='\0';i++){
+                cout<<myname[i];//looping i from 1st element to the last element(i.e) the element before \0
+        }
+        cout<<endl;
+        cout<<"Printing string myname using pointers: ";
+        char*p=&myname[0];
+        for(int i=0;*(p+i)!='\0';i++){
+                cout<<*(p+i);//looping for all pointers from address of first element to the last element
+        }
+        cout<<endl;
+        //terminate the fn
+        return 777;
+}
